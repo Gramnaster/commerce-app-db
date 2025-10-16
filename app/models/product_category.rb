c03adc46b1587cd5670d745342d4ee37f1c7bf0b
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-16 23:37:42 +0800

    ProductCategory: Adds basic model validation to product categories for uniqueness

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -2,4 +2,6 @@ class ProductCategory < ApplicationRecord
   has_many :products, dependent: :destroy
   has_many :promotions_categories, dependent: :destroy
   has_many :promotions, through: :promotions_categories
+
+  validates :title, presence: true, uniqueness: true
 end
