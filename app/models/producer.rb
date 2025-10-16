ae373e4c7ccbc4bc91c2ef473a927c5855b3e959
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-17 00:00:40 +0800

    Producer: Adds basic validations

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,4 +1,10 @@
 class Producer < ApplicationRecord
   belongs_to :address
   has_many :products, dependent: :destroy
+
+  validates :title, presence: true, uniqueness: true
+  validates :address, presence: true
+
+  # Accept nested attributes for address to allow creating/updating address with producer
+  accepts_nested_attributes_for :address, allow_destroy: false
 end
