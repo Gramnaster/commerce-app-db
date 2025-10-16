64e7fa2c59a319b08b3d0784548e31b4368e60bb
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-17 00:45:21 +0800

    Products: Adds Products routes

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
       resources :users, only: [ :index, :show, :update, :destroy ]
       resources :admin_users, only: [ :index, :show, :update, :destroy ]
       resources :countries, only: [ :index, :show ]
-      resources :products, only: [ :index, :show ]
+      resources :products, only: [ :index, :show, :create, :update, :destroy ]
       resources :product_categories, only: [ :index, :show, :create, :update, :destroy ]
       resources :producers, only: [ :index, :show, :create, :update, :destroy ]
       resources :promotions, only: [ :index, :show, :create, :update, :destroy ]
