71cd44a4216d1741fab7e9db6b835c4f059dbfd9
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-17 00:12:17 +0800

    Promotions: Adds Promotions validation

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -2,4 +2,6 @@ class Promotion < ApplicationRecord
   has_many :products, dependent: :nullify
   has_many :promotions_categories, dependent: :destroy
   has_many :product_categories, through: :promotions_categories
+
+  validates :discount_amount, presence: true, numericality: { greater_than: 0 }
 end
