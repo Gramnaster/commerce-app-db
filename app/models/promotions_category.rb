22ed65e27ae660527c5d2877d981bc503623de6a
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-17 00:20:57 +0800

    PromCat: Adds PromCat validations

--- app/models/promotions_category.rb
+++ app/models/promotions_category.rb
@@ -1,4 +1,8 @@
 class PromotionsCategory < ApplicationRecord
   belongs_to :product_category, foreign_key: :product_categories_id
   belongs_to :promotion, foreign_key: :promotions_id
+
+  validates :product_categories_id, presence: true
+  validates :promotions_id, presence: true
+  validates :product_categories_id, uniqueness: { scope: :promotions_id, message: "has already been associated with this promotion" }
 end
