06dccbd6262bed517825803072c2248c8fba437c
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-22 20:54:12 +0800

    Mailer: Adds debuggers to find out exactly where things are going wrong

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
   default from: ENV["GMAIL_USERNAME"] || "noreply@commerce.com"
   layout "mailer"
+
+  def mail(headers = {}, &block)
+    Rails.logger.debug "[ApplicationMailer] mail called: headers=#{headers.inspect}"
+    super
+  end
 end
--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -3,11 +3,21 @@ class DeviseMailer < Devise::Mailer
   default template_path: "devise/mailer" # to use the devise views
 
   def confirmation_instructions(record, token, opts = {})
-    # Build frontend confirmation URL
-    frontend_url = "#{ENV.fetch('FRONTEND_URL', 'http://localhost:5173')}/users/confirmation?confirmation_token=#{token}"
+    Rails.logger.debug "[DeviseMailer] === confirmation_instructions called ==="
+    Rails.logger.debug "[DeviseMailer] record: #{record.inspect}"
+    Rails.logger.debug "[DeviseMailer] token: #{token.inspect}"
+    Rails.logger.debug "[DeviseMailer] opts (before): #{opts.inspect}"
+    frontend_url = "#{ENV['FRONTEND_URL']}/users/confirmation?confirmation_token=#{token}"
+    Rails.logger.debug "[DeviseMailer] ENV['FRONTEND_URL']: #{ENV['FRONTEND_URL'].inspect}"
+    Rails.logger.debug "[DeviseMailer] frontend_url: #{frontend_url.inspect}"
     @confirmation_url = frontend_url
     @email = record.email
     opts[:confirmation_url] = frontend_url
-    super
+    Rails.logger.debug "[DeviseMailer] opts (after): #{opts.inspect}"
+    Rails.logger.debug "[DeviseMailer] @confirmation_url: #{@confirmation_url.inspect}"
+    Rails.logger.debug "[DeviseMailer] @email: #{@email.inspect}"
+    result = super
+    Rails.logger.debug "[DeviseMailer] === confirmation_instructions finished ==="
+    result
   end
 end
