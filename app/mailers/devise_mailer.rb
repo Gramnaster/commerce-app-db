190ef4b853e35f640ea40e8e6f2c87cac477fc74
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-22 14:55:42 +0800

    Fix: Mailer should redirect to frontend

---
+++ app/mailers/devise_mailer.rb
@@ -0,0 +1,11 @@
+class DeviseMailer < Devise::Mailer
+  helper :application # gives access to all helpers defined within `application_helper`.
+  default template_path: "devise/mailer" # to use the devise views
+
+  def confirmation_instructions(record, token, opts = {})
+    # Build frontend confirmation URL
+    frontend_url = "#{ENV.fetch('FRONTEND_URL', 'http://localhost:5173')}/users/confirmation?confirmation_token=#{token}"
+    opts[:confirmation_url] = frontend_url
+    super
+  end
+end
