da7edf7d6570c4c4602b255daba8a4d726f17d9d
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-23 14:36:34 +0800

    Fix: Adds mailer retries limitation to 1

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   default from: ENV["GMAIL_USERNAME"] || "noreply@commerce.com"
   layout "mailer"
+  retry_on StandardError, attempts: 1
 
   def mail(headers = {}, &block)
     Rails.logger.debug "[ApplicationMailer] mail called: headers=#{headers.inspect}"
