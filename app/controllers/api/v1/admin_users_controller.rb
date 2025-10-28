efdaf5c0ca666605e86b2112df0602ec10cb792b
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-29 00:14:07 +0800

    Admin users: Adds pagination to views

--- app/controllers/api/v1/admin_users_controller.rb
+++ app/controllers/api/v1/admin_users_controller.rb
@@ -57,7 +57,12 @@ class Api::V1::AdminUsersController < ApplicationController
       return render json: { error: "Unauthorized. Higher permissions required." }, status: :forbidden
     end
 
-  @admin_users = AdminUser.includes(:admin_detail, :admin_phones, { admin_addresses: :address }, company_sites: :address).all
+    admin_users = AdminUser.includes(:admin_detail, :admin_phones, { admin_addresses: :address }, company_sites: :address).all
+    
+    result = paginate_collection(admin_users, default_per_page: 20)
+    @admin_users = result[:collection]
+    @pagination = result[:pagination]
+    
     render :index
   end
 
