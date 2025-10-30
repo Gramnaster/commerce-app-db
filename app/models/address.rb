58f8133bf5cc323c142ab0767ab8d99c32a5e521
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-30 20:55:10 +0800

    Addresses: Updates model

--- app/models/address.rb
+++ app/models/address.rb
@@ -8,4 +8,16 @@ class Address < ApplicationRecord
   has_many :company_sites, dependent: :destroy
 
   validates :unit_no, :street_no, :barangay, :city, :zipcode, :country_id, presence: true
+  validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }, allow_nil: true
+  validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, allow_nil: true
+
+  # Check if address has been geocoded
+  def geocoded?
+    latitude.present? && longitude.present?
+  end
+
+  # Build full address string for geocoding
+  def full_address
+    [ unit_no, street_no, barangay, city, zipcode, country&.name ].compact.join(", ")
+  end
 end
