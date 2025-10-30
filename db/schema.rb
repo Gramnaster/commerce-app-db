e3fed5557ed321ee3dee0609216629ebf3eac39f
Gramnaster <jocvillalon@yahoo.com.ph>
2025-10-30 20:54:11 +0800

    Addresses: Adds geolocation columns for the Google Maps implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_10_29_053935) do
+ActiveRecord::Schema[8.1].define(version: 2025_10_30_125317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -57,6 +57,10 @@ ActiveRecord::Schema[8.1].define(version: 2025_10_29_053935) do
     t.string "city", null: false
     t.bigint "country_id", null: false
     t.datetime "created_at", null: false
+    t.string "geocode_source"
+    t.datetime "geocoded_at"
+    t.decimal "latitude", precision: 10, scale: 8
+    t.decimal "longitude", precision: 11, scale: 8
     t.string "region"
     t.string "street_no", null: false
     t.string "unit_no", null: false
